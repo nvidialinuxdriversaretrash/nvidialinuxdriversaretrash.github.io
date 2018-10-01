f441d1ded298b5a0163ca74879b57fab921f58d7
Kristopher Ives <kristopher.ives@gmail.com>
2018-09-30 23:22:27 -0700

    Changed addstack.sh helper script to collapse dashes.

--- addstack.sh
+++ addstack.sh
@@ -6,7 +6,7 @@ title=$(nokogiri tmp.html -e 'puts $_.at_css("title").content')
 date=$(nokogiri tmp.html -e 'puts $_.at_css(".post-signature.owner .relativetime").attr("title")')
 date=$(date -d "$date" +%Y-%m-%d)
 words=( $title )
-title=$(echo "${words[@]:0:10}" | tr ' ' - | sed -E 's/^-+//' | sed -E 's/-+$//')
+title=$(echo "${words[@]:0:10}" | tr ' ' - | sed -E 's/-+/-/g' | sed -E 's/^-+//' | sed -E 's/-+$//')
 file="files/$date-$title.html"
 
 if [ -f "$file" ]; then
